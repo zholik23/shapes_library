f6a0d1d9efa2a37a67c59b331693a6186275a237
zholik23 <131287817+zholik23@users.noreply.github.com>
2024-07-22 13:46:55 +0900

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,41 @@
+import math
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+class Triangle(Shape):
+    def __init__(self, side1, side2, side3):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+
+    def area(self):
+        s = (self.side1 + self.side2 + self.side3) / 2
+        return math.sqrt(s * (s - self.side1) * (s - self.side2) * (s - self.side3))
+
+    def is_right_triangle(self):
+        sides = sorted([self.side1, self.side2, self.side3])
+        return math.isclose(sides[0]**2 + sides[1]**2, sides[2]**2)
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+def calculate_area(shape=None):
+    if shape is None:
+        raise ValueError("Фигура не была задана")
+    return shape.area()
